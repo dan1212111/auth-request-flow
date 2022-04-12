dee77a6227a6694c785e63150f281f8beefbeaa5
Daniel Mc <danielmc122333@icloud.com>
2022-04-12 10:44:38 +0100

    completed

--- src/router.js
+++ src/router.js
@@ -1,24 +1,40 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+/** @format */
 
-const router = express.Router();
+const express = require("express")
+const jwt = require("jsonwebtoken")
+
+const router = express.Router()
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
-};
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
+}
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const token = jwt.sign({ username: mockUser.username }, "secret")
+  res.json({ token })
+})
 
-});
+router.get("/profile", (req, res) => {
+  const token = req.headers["authorization"]
+  try {
+    const payload = jwt.verify(token, secret)
+    res.json({profile: mockUser.profile})
+  } catch (e) {
+    res.status(401)
+    res.json({error: 'token not valid'})
+    return
+  }
+  res.json({ test: true })
+})
 
-router.get('/profile', (req, res) => {
+module.exports = router
 
-});
-
-module.exports = router;
+// 1. Create a new JWT; the payload should be an object containing the mock user's username.
+//     - Don't worry about working with credentials right now.
+// 2. Send the JWT back to the client in a JSON response.
